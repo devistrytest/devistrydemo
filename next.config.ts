4794ef7a95946b20820f141f4fe9cf1a6eb53e04
jgbijlsma <jbijlsma.p@gmail.com>
2024-11-08 18:06:31 +0100

    add allowedOrigin for server action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["178.128.246.156"],
+    },
+  },
 };
 
 export default nextConfig;
